78eb660c141619bba9660a6799a66b1f404fc376
Pasa02001 <54437734+Pasa02001@users.noreply.github.com>
2020-10-10 20:35:44 +0200

    Add files via upload

---
+++ Monogame/Game1/Game1/Game1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D cursor;
+        Texture2D bg;
+        Vector2 mousePos;
+
+        Player player;
+        //Laser laser;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.IsFullScreen = true;
+            IsMouseVisible = false;
+            graphics.ApplyChanges();
+            
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D Eye = Content.Load<Texture2D>("Eye");
+            cursor = Content.Load<Texture2D>("cursor");
+            Vector2 Eyepos = new Vector2(100, 100);
+            player = new Player(Eye);
+            //laser = new Laser(LaserT);
+
+            bg = Content.Load<Texture2D>("space");
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mousePos = Mouse.GetState().Position.ToVector2();
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+            player.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            spriteBatch.Draw(bg, new Rectangle(0, 0, 1920, 1080), Color.White);
+            player.Draw(spriteBatch);
+            //laser.Draw(spriteBatch);
+            spriteBatch.Draw(cursor, new Rectangle((int)mousePos.X, (int)mousePos.Y - 15, cursor.Width, cursor.Height), Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+
+
+        }
+    }
+}
---
+++ Monogame/Game1/Game1/Laser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class Laser
+    {
+        Texture2D LaserT;
+        Vector2 LaserPos = new Vector2(500, 500);
+        Vector2 Eyepos = new Vector2(300, 300);
+        Vector2 mousePos;
+        float angle = 0;
+        
+
+        public Laser(Texture2D texture)
+        {
+            LaserT = texture;
+
+        }
+
+        public void Update(GameTime time)
+        {
+            KeyboardState ks = Keyboard.GetState();
+            if (ks.IsKeyDown(Keys.W))
+            {
+                LaserPos.Y -= 10;
+            }
+            if (ks.IsKeyDown(Keys.S))
+            {
+                LaserPos.Y += 10;
+            }
+            if (ks.IsKeyDown(Keys.A))
+            {
+                LaserPos.X -= 10;
+            }
+            if (ks.IsKeyDown(Keys.D))
+            {
+                LaserPos.X += 10;
+            }
+
+            mousePos = Mouse.GetState().Position.ToVector2();
+            angle = (float)Math.Atan2(Eyepos.Y - mousePos.Y, Eyepos.X - mousePos.X);
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+            }
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //float scale = .5f;
+
+            //spriteBatch.Draw(LaserT, new Rectangle((int)LaserPos.X, (int)LaserPos.Y, (int)(LaserT.Width * scale), (int)(LaserT.Height * scale)), null, Color.White, angle, new Vector2(LaserT.Width / 2, LaserT.Height / 2), SpriteEffects.None, 0);
+        }
+    }
+}
